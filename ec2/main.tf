3cd85970b34fa10c948006c7ff923b537e1b5391
Mukesh <Stunty@007>
2023-02-13 22:09:24 +0530

    learning devops

---
+++ ec2/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "baston" {
+  ami = "ami-0bb6af715826253bf"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [var.SG_ID]
+  tags = {
+    Name = "baston"
+  }
+}
+
+variable "SG_ID" {}
+
+output  "Private_IP" {
+  value = "${aws_instance.baston.public_ip}"
+}
--- main.tf
+++ main.tf
@@ -1,14 +1,8 @@
+module "ec2" {
+  source = "./ec2"
+  SG_ID = module.sg.SG_ID
+}
 
-resource "aws_instance" "baston" {
- ami = "ami-0bb6af715826253bf"
- instance_type = "t2.micro"
- associate_public_ip_address = true
- tags = {
-   Name = "baston"
-   }
-    }
-
-
-  output  "Private_IP" {
-   value = "${aws_instance.baston.public_ip}"
-    }
+module "sg" {
+  source = "./sg"
+}
