1255a6a8c230fee84029a78f603ba150e3e6f3c8
Mukesh <Stunty@007>
2023-02-13 22:20:50 +0530

    learning devops

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "baston" {
   ami = "ami-0bb6af715826253bf"
-  count = 2
   instance_type = "t2.micro"
   associate_public_ip_address = true
   vpc_security_group_ids = [var.SG_ID]
