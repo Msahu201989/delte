1fd1f36196d14e8462f8c2d7a121a1451f896e61
Mukesh <Stunty@007>
2023-02-19 11:58:16 +0530

    learning devops

--- main.tf
+++ main.tf
@@ -1,8 +1,26 @@
-module "ec2" {
-  source = "./ec2"
-  SG_ID = module.sg.SG_ID
+resource "aws_instance" "Baston" {
+  ami = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "baston"
+  }
+
+
 }
 
-module "sg" {
-  source = "./sg"
-}
+
+
+
+
+
+
+
+#module "ec2" {
+#  source = "./ec2"
+#  SG_ID = module.sg.SG_ID
+#}
+#
+#module "sg" {
+#  source = "./sg"
+#}
